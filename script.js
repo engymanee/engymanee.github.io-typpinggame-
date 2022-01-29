347781f3d7675b53f4c8a1ab95c7d7f20f4573db
engymanee <ngozi.nnamani@gmail.com>
2022-01-29 22:24:36 +0100

    updaed css and game

---
+++ script.js
@@ -0,0 +1,58 @@
+const quote  = document.getElementById('quote');
+const input = document.getElementById('typed-value');
+const start = document.getElementById('start');
+const message = document.getElementById('message');
+let highlightPosition;
+let wordQueue;
+let startTime;
+
+
+function startGame(){
+            console.log('game started!');
+
+            quoteText = 'type me now';
+            wordQueue = quoteText.split(' ');
+            quote.innerHTML = wordQueue.map(word=>(`<span>${word}</span>`)).join('');
+
+            highlightPosition = 0;
+            quote.childNodes[highlightPosition].className = 'highlight';
+            startTime = new Date().getTime();
+}
+
+function checkInput (){
+    console.log('checking input');
+    const currentWord = wordQueue[0]; //sets the first word in array to currentWord
+    const typedValue = input.value.trim();//gets value from input and removes spaces.
+
+    if (currentWord !== typedValue){
+        input.className = currentWord.startsWith(typedValue) ? "" : 'error';
+        return;
+    }
+
+    wordQueue.shift(); //removes the first item in the word queue
+    input.value = '';
+
+    quote.childNodes[highlightPosition].className = "";
+       
+
+    if (wordQueue.length === 0){
+        gameOver();
+        return;
+    }
+
+    highlightPosition++;
+    quote.childNodes[highlightPosition].className = "highlight"; 
+}
+
+
+function gameOver(){
+    const endGameTime = new Date().getTime() - startTime;
+    document.body.className = "winner";
+    message.innerHTML = `<span class="congrats">Congratulations</span>
+    </br>
+    You finished in ${endGameTime/1000} seconds`;
+
+}
+
+start.addEventListener('click',startGame);
+input.addEventListener('input',checkInput);
